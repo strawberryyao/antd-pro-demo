746004ca27b054ece1813d77dc3ff9d49d4d70c4
strawberry <strawberry.wu@newtype.io>
2020-07-01 18:18:26 +0800

    first commit

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Header />
+    <SiderMenu />
+    <Home />
+    <router-view></router-view>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
+import SiderMenu from "./SiderMenu.vue";
+import Home from "./Home.vue";
+export default {
+  components: {
+    Header,
+    Footer,
+    Home,
+    SiderMenu
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/router/index.js
@@ -0,0 +1,93 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import NotFound from "../views/404.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/user",
+    component: () =>
+      import(/* webpackChunkNameuser: "layouts" */ "../layouts/UserLayout.vue"),
+    children: [
+      {
+        path: "/user",
+        redirect: "/user/login"
+      },
+      {
+        path: "/user/login",
+        name: "login",
+        component: () =>
+          import(/* webpackChunkName: "user" */ "../views/User/Login.vue")
+      },
+      {
+        path: "/user/register",
+        name: "register",
+        component: () =>
+          import(/* webpackChunkName: "user" */ "../views/User/Register.vue")
+      }
+    ]
+  },
+  {
+    path: "/",
+    component: () =>
+      import(/* webpackChunkName: "layout" */ "../layouts/BasicLayout.vue"),
+    children: [
+      // dashboard
+      {
+        path: "/",
+        redirect: "/dashboard/home"
+      },
+      {
+        path: "/dashboard",
+        name: "dashboard",
+        // component: { render: h => h("router-view") },
+        children: [
+          {
+            path: "/dashboard/home",
+            name: "home",
+            component: () =>
+              import(/* webpackChunkName: "layout" */ "../layouts/Home.vue")
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: "/form",
+    name: "form",
+    component: { render: h => h("router-view") },
+    children: [
+      {
+        path: "/form/basic-form",
+        name: "basicform",
+        component: () =>
+          import(/* webpackChunkName: "forms" */ "../views/Forms/BasicForm.vue")
+      }
+    ]
+  },
+  {
+    path: "*",
+    name: "404",
+    component: NotFound
+  }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
+});
+
+router.beforeEach((to, form, next) => {
+  NProgress.start();
+  next();
+});
+
+router.afterEach(() => {
+  NProgress.done();
+});
+
+export default router;
